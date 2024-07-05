aa5825b1164c16d27e141b91a17a25dc7c68cce2
Sietse Andringa <andringa.sietse@gmail.com>
2024-07-05 18:08:11 +0200

    Daguitslag test

---
+++ testscript.js
@@ -0,0 +1,7 @@
+const storyHeading = document.querySelector("#story-heading");
+
+const testText = "Hello, this is a test";
+
+const testHtml = "<h3>This is a H3 heading</h3>";
+
+storyHeading.insertAdjacentHTML("beforeend", testHtml);
